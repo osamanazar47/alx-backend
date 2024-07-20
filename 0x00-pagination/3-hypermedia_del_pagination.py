86b041af6bf0f506cd9490cca3a30f1ae561ffe4
osamanazar47 <osamanazar47@gmail.com>
2024-07-20 17:53:32 +0300

    task 3 finished style fix

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -37,7 +37,8 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Returns a dictionary with these keys: index, next_index, page_size, and data."""
+        """Returns a dictionary with these keys:
+        index, next_index, page_size, and data."""
         assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
 
