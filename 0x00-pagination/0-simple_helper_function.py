001fa630082164de2663a49e79dec87f0a31ab6f
osamanazar47 <osamanazar47@gmail.com>
2024-07-20 14:26:08 +0300

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for task 0"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Returns a tuple of size two containing start and end indexes"""
+    start =int((page - 1) * page_size)
+    end = int(start + page_size)
+    t = (start, end)
+    return t
