15255475965dbe37d573f0d43b3359fa48cccff6
osamanazar47 <osamanazar47@gmail.com>
2024-07-20 17:51:20 +0300

    task 3 finished

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -61,12 +61,3 @@ class Server:
             'data': page_data,
         }
         return page_info
-
-
-def index_range(page: int, page_size: int) -> tuple:
-    """
-    Returns a tuple of size two containing start and end indexes"""
-    start = int((page - 1) * page_size)
-    end = int(start + page_size)
-    t = (start, end)
-    return t
