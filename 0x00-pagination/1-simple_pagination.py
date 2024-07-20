e469749531032a5cc997d276ebc7cdec5cd38075
osamanazar47 <osamanazar47@gmail.com>
2024-07-20 14:47:14 +0300

    task 1 style fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -25,7 +25,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Takes two integer arguments page with default value 1 and page_size with default value 10."""
+        """Takes two integer arguments page with
+        default value 1 and page_size with default value 10."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
