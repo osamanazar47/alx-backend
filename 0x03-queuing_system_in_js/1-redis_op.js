9a8eb3fe319db332d4a4eda935bcdafe111e167c
osamanazar47 <osamanazar47@gmail.com>
2024-08-27 12:00:56 +0300

    added two new methods for setting and retrieving values from redis

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,1 +1,29 @@
-#!/usr/bin/node
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client
+  .on('ready', () => {
+    console.log('Redis client connected to the server')
+  })
+  .on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error}`)
+  })
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, value) => {
+    if (err) {
+      console.log(err);
+	} else {
+      console.log(value);
+	}
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
