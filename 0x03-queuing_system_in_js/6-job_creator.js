8010d9d1f2d3768ac4fd30518875e9d426ac2141
osamanazar47 <osamanazar47@gmail.com>
2024-08-27 18:32:39 +0300

    created first job using Kue

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+const kue = require('kue');
+const redis = require('redis');
+
+const job_data = {
+  phoneNumber: '012345678',
+  message: 'Hello from Kue',
+};
+const push_notification_code = kue.createQueue()
+
+const job = push_notification_code.create('data', job_data).save((err) => {
+  if (!err) {
+    console.log(`Notification job created: ${job.id}`);
+	job.on('complete', () => {
+      console.log('Notification job completed');
+    });
+
+	job.on('failed', () => {
+      console.log('Notification job failed');
+	});
+  }
+});
