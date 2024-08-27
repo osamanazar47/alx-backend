4b523067bdf7959b1c554aec08bb08a95f9a9f20
osamanazar47 <osamanazar47@gmail.com>
2024-08-27 13:20:07 +0300

    added hashes but with callbacks

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -1,1 +1,27 @@
-#!/usr/bin/node
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client
+  .on('ready', async () => {
+    console.log('Redis client connected to the server')
+    await main();
+  })
+  .on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error}`)
+  })
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = async (schoolName) => {
+  console.log(await promisify(client.GET).bind(client)(schoolName));
+};
+
+async function main() {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
