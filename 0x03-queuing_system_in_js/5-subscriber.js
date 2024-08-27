8ca16b5e65f3cf059f4435f7b9ac633679b29176
osamanazar47 <osamanazar47@gmail.com>
2024-08-27 17:37:35 +0300

    set up a subscriber and a publisher

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+import redis from 'redis';
+
+const client = redis.createClient()
+
+client
+  .on('connect', () => {
+    console.log('Redis client connected to the server');
+	client.subscribe('holberton school channel');
+  })
+  .on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+  })
+  .on('message', (channel, message) => {
+    console.log(message);
+    if (message === 'KILL_SERVER') {
+      client.unsubscribe('holberton school channel');
+	  client.quit();
+    }
+  });
