4bc8d2bc237d49fdf8aff1282826c1b68091905c
osamanazar47 <osamanazar47@gmail.com>
2024-08-28 10:30:00 +0300

    created job creator and processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,21 +1,20 @@
-const kue = require('kue');
-const redis = require('redis');
+import { createQueue } from 'kue';
 
-const job_data = {
-  phoneNumber: '012345678',
-  message: 'Hello from Kue',
-};
-const push_notification_code = kue.createQueue()
+const queue = createQueue({name: 'push_notification_code'});
 
-const job = push_notification_code.create('data', job_data).save((err) => {
-  if (!err) {
-    console.log(`Notification job created: ${job.id}`);
-	job.on('complete', () => {
-      console.log('Notification job completed');
-    });
-
-	job.on('failed', () => {
-      console.log('Notification job failed');
-	});
-  }
+const job = queue.create('push_notification_code', {
+  phoneNumber: '07045679939',
+  message: 'Account registered',
 });
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+job.save();
